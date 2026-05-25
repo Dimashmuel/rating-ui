72a5e4b9ec4a4a185ace72bc3c525935b5181dbb
dimashmuel <dimashmuel12@gmail.com>
2026-05-25 19:16:15 +0300

    Add Props

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import Rating from './components/Rating';
 
 
 const App = () => {
-  return  <div>
-    <Rating />
+  return <div>
+    <Rating
+      heading='How do you feel about React?'
+      feedbackMessages={['Hate it', 'Dislike', 'Neutral', 'Like it', 'Love it']}
+    />
   </div>;
 };
- 
+
 export default App;
